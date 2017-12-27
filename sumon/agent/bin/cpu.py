f1977be611de75e99365887570467b14cd116a50
liuhui <liuhui_py@163.com>
2017-12-27 22:08:19 +0800

    add cpu.py

--- sumon/agent/bin/cpu.py
+++ sumon/agent/bin/cpu.py
@@ -3,6 +3,22 @@
 import os
 from subprocess import Popen, PIPE
 
+import psutil
 
-def cpu():
-    pass
+def get_cpu():
+    sys_cpu = {}
+    cpu_time = psutil.cpu_times_percent(interval=1)
+    sys_cpu['percent'] = psutil.cpu_percent(interval=1)
+    sys_cpu['lcpu_percent'] = psutil.cpu_percent(interval=1, percpu=True)
+    sys_cpu['user'] = cpu_time.user
+    sys_cpu['nice'] = cpu_time.nice
+    sys_cpu['system'] = cpu_time.system
+    sys_cpu['idle'] = cpu_time.idle
+    sys_cpu['iowait'] = cpu_time.iowait
+    sys_cpu['irq'] = cpu_time.irq
+    sys_cpu['softirq'] = cpu_time.softirq
+    sys_cpu['guest'] = cpu_time.guest
+    return sys_cpu
+
+getCpu = get_cpu()
+print(getCpu)
