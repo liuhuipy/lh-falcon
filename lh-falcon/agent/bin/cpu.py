fdba45870dc242752af87fae1b3a9110899471f7
liuhui <liuhui_py@163.com>
2018-02-05 21:55:55 +0800

    update cpu.py to get system_cpu_info

--- lh-falcon/agent/bin/cpu.py
+++ lh-falcon/agent/bin/cpu.py
@@ -1,6 +1,6 @@
 # coding:utf8
-
 import os
+import json
 from subprocess import Popen, PIPE
 
 import psutil
@@ -8,14 +8,20 @@ import psutil
 def get_cpu():
     sys_cpu = {}
     cpu_time = psutil.cpu_times_percent(interval=1)
-    sys_cpu['percent'] = psutil.cpu_percent(interval=1)
-    sys_cpu['lcpu_percent'] = psutil.cpu_percent(interval=1, percpu=True)
-    sys_cpu['user'] = cpu_time.user
-    sys_cpu['nice'] = cpu_time.nice
-    sys_cpu['system'] = cpu_time.system
-    sys_cpu['idle'] = cpu_time.idle
-    sys_cpu['iowait'] = cpu_time.iowait
-    sys_cpu['irq'] = cpu_time.irq
-    sys_cpu['softirq'] = cpu_time.softirq
-    sys_cpu['guest'] = cpu_time.guest
+    sys_cpu['cpu_num'] = psutil.cpu_count()
+    sys_cpu['phyical_cpu_num'] = psutil.cpu_count(logical=False)
+    sys_cpu['user_time'] = cpu_time.user
+    sys_cpu['nice_time'] = cpu_time.nice
+    sys_cpu['system_time'] = cpu_time.system
+    sys_cpu['idle_time'] = cpu_time.idle
+    sys_cpu['iowait_time'] = cpu_time.iowait
+    sys_cpu['irq_time'] = cpu_time.irq
+    sys_cpu['softirq_time'] = cpu_time.softirq
+    sys_cpu['guest_time'] = cpu_time.guest
     return sys_cpu
+
+
+#if __name__ == '__main__':
+#    system_cpu = get_cpu()
+#    system_cpu_info = json.dumps(system_cpu)
+#    print(system_cpu_info)
